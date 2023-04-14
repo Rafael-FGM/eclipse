4bc97b49afc85543ce3f7dbbd2676f387dfad3ef
Rafael Ferreira <130174999+Rafael-FGM@users.noreply.github.com>
2023-04-14 11:09:46 -0300

    Adicionei arquivos via upload

---
+++ GitHub/src/br/com/rafaelferreira/git/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.rafaelferreira.git;
+
+/**
+ * @author Desktop
+ * Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+
+	}
+
+}
